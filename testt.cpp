01465744b8ef049651f4f5b396efcea880f52d00
AdityaTheJaat <programmingID1306@gmail.com>
2022-12-28 19:28:27 +0530

    testt vaali file

---
+++ testt.cpp
@@ -0,0 +1,22 @@
+//<<<<<<<<<<<<Author>>>>>>>>>>>>>
+//<<<<<<<<<adityathejaat>>>>>>>>>
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define lli long long int
+#define ll long long
+#define inn int n; cin>>n;
+#define test lli t ; cin>>t; while(t--)
+#define li for(int i=0; i<n; i++)
+#define lj for(int j=0; j<n; j++)
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+        cout<<"HELLO";
+        return 0;
+}
+
+//<<<<<<<<<<<<CodedBy>>>>>>>>>>>
+//<<<<<<<<AdityaChaudhary>>>>>>>
